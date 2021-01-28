d82b6c66b2141ba70285637b23c6fe11f5acc5b4
gf <gmessori@gmail.com>
2021-01-28 12:00:12 +0100

    Configurazione da "environment variable" o da ".env" anziche` da site_config.py

--- config.py
+++ config.py
@@ -1,36 +1,42 @@
 import pprint
 import os
+import wsgian
 
 def config(pard):
-	import site_config
-	
-	for k in [
-			'HTTP_PROTOCOL',
-			'HTTP_ADDRESS',
-			'HTTP_PORT',
-			'db.mysql',
-			'SMTP_HOST',
-			'SMTP_PORT',
-			'SMTP_USER',
-			'SMTP_PASSW',
-			'DEFAULT_SENDER',
-			'TEMPLATE_RELOAD',
-			'with_static',
-			'with_ws',
-			'ws_auth',
-			'ws_version',
-			'cookie_secret',
-			'DEBUG',
-			'LOGLEVEL',
-			'GOOGLE',
-			'EMAIL',
-			]:
-		pard[k] = site_config.config.get(k)
+	default_config = {
+		'HTTP_PROTOCOL': 'http',
+		'HTTP_ADDRESS': 'localhost',
+		'HTTP_PORT': '80',
+		'db.mysql': {},
+		'SMTP_HOST': '',
+		'SMTP_PORT': '',
+		'SMTP_USER': '',
+		'SMTP_PASSW': '',
+		'DEFAULT_SENDER': '',
+		'TEMPLATE_RELOAD': False,
+		'with_static': True,
+		'with_ws': False,
+		'ws_auth': False,
+		'ws_version': 2,
+		'cookie_secret': 'notapplicable',
+		'DEBUG': False,
+		'LOGLEVEL': 'debug',
+		'GOOGLE': {},
+		'EMAIL': {},
+		}
+	wsgian.utils.dot_env()
+	for k in default_config:
+		pard[k] = os.environ.get(k, default_config[k])
 	
 	pard['APPLICATION_NAME'] = 'WiFiCode'
 	
-	_HTTP_PORT = ':' + str(pard['HTTP_PORT']) if pard['HTTP_PORT'] != 80 else ''
-	pard['APPSERVER'] = '%(HTTP_PROTOCOL)s://%(HTTP_ADDRESS)s' % pard + _HTTP_PORT
+	pard['APP_URL'] = os.environ.get('APP_URL')
+	
+	if pard['APP_URL']:
+		pard['APPSERVER'] = pard['APP_URL']
+	else:
+		_HTTP_PORT = ':' + str(pard['HTTP_PORT']) if pard['HTTP_PORT'] != 80 else ''
+		pard['APPSERVER'] = '%(HTTP_PROTOCOL)s://%(HTTP_ADDRESS)s' % pard + _HTTP_PORT
 	
 	pard['PROJECT'] = 'wificode'
 	pard['PROJECT_SUBTITLE'] = 'Generatore di QRCode per la configurazione automatica della rete wi-fi'
@@ -39,3 +45,8 @@ def config(pard):
 	
 	return pard
 
+
+if __name__ == '__main__':
+	import pprint
+	pard = config({})
+	pprint.pprint(pard)
